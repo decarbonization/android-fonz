e8806920562ce2e480c94b10c7f38c16e43d82fc
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2015-11-02 21:52:53 -0800

    database for persisting scores. #1

---
+++ app/src/main/java/com/kevinmacwhinnie/fonz/data/ScoresStore.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2015, Peter 'Kevin' MacWhinnie
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions may not be sold, nor may they be used in a commercial
+ *    product or activity.
+ * 2. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 3. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.kevinmacwhinnie.fonz.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
+
+public class ScoresStore extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "HighScoresDB";
+    public static final int VERSION = 1;
+
+    public static final int SCORE_LIMIT = 12;
+    public static final String TABLE_SCORES = "Scores";
+
+    //region Columns
+
+    public static final String COLUMN_NAME = "name";
+    public static final int COLUMN_NAME_INDEX = 0;
+
+    public static final String COLUMN_SCORE = "score";
+    public static final int COLUMN_SCORE_INDEX = 1;
+
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+    public static final int COLUMN_TIMESTAMP_INDEX = 2;
+
+    public static final String NAME_DEFAULT = "--";
+    public static final int SCORE_DEFAULT = 0;
+
+    //endregion
+
+
+    //region Lifecycle
+
+    public ScoresStore(@NonNull Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        final String createTables =
+                ("CREATE TABLE IF NOT EXISTS " + TABLE_SCORES + " (" +
+                        COLUMN_NAME + " TEXT, " +
+                        COLUMN_SCORE + " INTEGER, " +
+                        COLUMN_TIMESTAMP + " INTEGER" +
+                 ")");
+        database.execSQL(createTables);
+
+        insertDefaults(database);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+    }
+
+    private void insertDefaults(@NonNull SQLiteDatabase database) {
+        final long now = System.currentTimeMillis();
+        for (int i = 0; i < Scores.NUMBER_SCORES; i++) {
+            final ContentValues values = new ContentValues(4);
+            values.put(COLUMN_NAME, NAME_DEFAULT);
+            values.put(COLUMN_SCORE, SCORE_DEFAULT);
+            values.put(COLUMN_TIMESTAMP, now);
+            database.insert(TABLE_SCORES, null, values);
+        }
+    }
+
+    @VisibleForTesting
+    void removeOutdatedEntries() {
+        final SQLiteDatabase database = getWritableDatabase();
+        final String delete =
+                ("DELETE FROM " + TABLE_SCORES + " WHERE ROWID NOT IN " +
+                 "(SELECT ROWID FROM " + TABLE_SCORES +
+                 " ORDER BY ROWID DESC LIMIT " + Scores.NUMBER_SCORES + ")");
+        database.execSQL(delete);
+    }
+
+    //endregion
+
+
+    public boolean isNewHighScore(int newScore) {
+        final SQLiteDatabase database = getReadableDatabase();
+        final String[] selectArguments = { Integer.toString(newScore, 10) };
+        final long count = DatabaseUtils.queryNumEntries(database,
+                                                         TABLE_SCORES,
+                                                         COLUMN_SCORE + " < ?",
+                                                         selectArguments);
+        return (count > 0);
+    }
+
+    public boolean insert(@NonNull String name, int score) {
+        final SQLiteDatabase database = getWritableDatabase();
+        final ContentValues values = new ContentValues(3);
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_SCORE, score);
+        values.put(COLUMN_TIMESTAMP, System.currentTimeMillis());
+        if (database.insert(TABLE_SCORES, null, values) != -1) {
+            removeOutdatedEntries();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void clear() {
+        final SQLiteDatabase database = getWritableDatabase();
+        database.delete(TABLE_SCORES, null, null);
+        insertDefaults(database);
+    }
+
+    public Cursor queryHighScores() {
+        final SQLiteDatabase database = getReadableDatabase();
+        final String[] columns = { COLUMN_NAME, COLUMN_SCORE, COLUMN_TIMESTAMP };
+        final String selection = null;
+        final String orderBy = "ROWID DESC";
+        return database.query(TABLE_SCORES,
+                              columns,
+                              selection,
+                              null, null, null,
+                              orderBy,
+                              Integer.toString(SCORE_LIMIT, 10));
+    }
+}
---
+++ app/src/test/java/com/kevinmacwhinnie/fonz/data/ScoresStoreTests.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2015, Peter 'Kevin' MacWhinnie
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions may not be sold, nor may they be used in a commercial
+ *    product or activity.
+ * 2. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 3. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.kevinmacwhinnie.fonz.data;
+
+import android.database.Cursor;
+
+import com.kevinmacwhinnie.fonz.FonzTestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+public class ScoresStoreTests extends FonzTestCase {
+    private final ScoresStore scoresStore = new ScoresStore(getContext());
+
+    @Before
+    public void setUp() {
+        scoresStore.removeOutdatedEntries();
+    }
+
+    @After
+    public void tearDown() {
+        scoresStore.clear();
+    }
+
+
+    @Test
+    public void insert() {
+        assertThat(scoresStore.insert("Dina", 500), is(true));
+        assertThat(scoresStore.insert("Gene", 900), is(true));
+        assertThat(scoresStore.insert("Louise", 9000), is(true));
+    }
+
+    @Test
+    public void clear() {
+        assertThat(scoresStore.insert("Louise", 9000), is(true));
+
+        final Cursor highScoresBefore = scoresStore.queryHighScores();
+
+        highScoresBefore.moveToNext();
+        assertThat(highScoresBefore.getString(ScoresStore.COLUMN_NAME_INDEX),
+                   is(equalTo("Louise")));
+        assertThat(highScoresBefore.getInt(ScoresStore.COLUMN_SCORE_INDEX),
+                   is(equalTo(9000)));
+        assertThat(highScoresBefore.getInt(ScoresStore.COLUMN_TIMESTAMP_INDEX),
+                   is(not(equalTo(0))));
+
+        highScoresBefore.close();
+
+        scoresStore.clear();
+        final Cursor highScoresAfter = scoresStore.queryHighScores();
+        while (highScoresAfter.moveToNext()) {
+            assertThat(highScoresAfter.getString(ScoresStore.COLUMN_NAME_INDEX),
+                       is(equalTo(ScoresStore.NAME_DEFAULT)));
+            assertThat(highScoresAfter.getInt(ScoresStore.COLUMN_SCORE_INDEX),
+                       is(equalTo(ScoresStore.SCORE_DEFAULT)));
+            assertThat(highScoresAfter.getInt(ScoresStore.COLUMN_TIMESTAMP_INDEX),
+                       is(not(equalTo(0))));
+        }
+    }
+
+    @Test
+    public void isNewHighScore() {
+        for (int i = 0; i < ScoresStore.SCORE_LIMIT; i++) {
+            assertThat(scoresStore.insert("Dina", 500), is(true));
+        }
+
+        assertThat(scoresStore.isNewHighScore(499), is(false));
+        assertThat(scoresStore.isNewHighScore(300), is(false));
+        assertThat(scoresStore.isNewHighScore(20), is(false));
+
+        assertThat(scoresStore.isNewHighScore(501), is(true));
+        assertThat(scoresStore.isNewHighScore(800), is(true));
+        assertThat(scoresStore.isNewHighScore(9000), is(true));
+    }
+
+    @Test
+    public void queryHighScores() {
+        assertThat(scoresStore.insert("Dina", 500), is(true));
+        assertThat(scoresStore.insert("Gene", 800), is(true));
+        assertThat(scoresStore.insert("Louise", 9000), is(true));
+
+        final Cursor highScores = scoresStore.queryHighScores();
+
+        highScores.moveToNext();
+        assertThat(highScores.getString(ScoresStore.COLUMN_NAME_INDEX),
+                   is(equalTo("Louise")));
+        assertThat(highScores.getInt(ScoresStore.COLUMN_SCORE_INDEX),
+                   is(equalTo(9000)));
+        assertThat(highScores.getInt(ScoresStore.COLUMN_TIMESTAMP_INDEX),
+                   is(not(equalTo(0))));
+
+        highScores.moveToNext();
+        assertThat(highScores.getString(ScoresStore.COLUMN_NAME_INDEX),
+                   is(equalTo("Gene")));
+        assertThat(highScores.getInt(ScoresStore.COLUMN_SCORE_INDEX),
+                   is(equalTo(800)));
+        assertThat(highScores.getInt(ScoresStore.COLUMN_TIMESTAMP_INDEX),
+                   is(not(equalTo(0))));
+
+        highScores.moveToNext();
+        assertThat(highScores.getString(ScoresStore.COLUMN_NAME_INDEX),
+                   is(equalTo("Dina")));
+        assertThat(highScores.getInt(ScoresStore.COLUMN_SCORE_INDEX),
+                   is(equalTo(500)));
+        assertThat(highScores.getInt(ScoresStore.COLUMN_TIMESTAMP_INDEX),
+                   is(not(equalTo(0))));
+
+        highScores.close();
+    }
+}
